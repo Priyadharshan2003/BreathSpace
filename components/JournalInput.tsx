be67924a39c9611a77d02b2a89c2379895d60745
Priyadharshan <priyan2003chandru@gmail.com>
2026-06-13 13:06:52 +0530

    style: fix multiline text input cursor alignment and remove web outline

--- components/JournalInput.tsx
+++ components/JournalInput.tsx
@@ -61,5 +61,8 @@ const styles = StyleSheet.create({
     color: theme.colors.light.text,
     lineHeight: theme.typography.lineHeights.body * 1.2,
     textAlignVertical: 'top', // For Android
+    paddingTop: theme.spacing.sm, // Forces top alignment on iOS/Web
+    // @ts-ignore
+    outlineStyle: 'none', // Removes web blue border
   },
 });
